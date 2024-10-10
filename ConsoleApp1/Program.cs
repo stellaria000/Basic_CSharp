d0d395318f8eee7fb804d5ae4f0a0347f5db13dc
syyou <syyou@pintel.co.kr>
2024-10-10 14:00:12 +0900

    Added example for Constructor/Overloading/Destructor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,71 @@
 ﻿using System;
 
-class Person
-{
-    public string Name;
-    public string Birthday;
-    public string Gender;
-
-    public void Eat()
-    {
-        Console.WriteLine(Name + "이(가) 아침을 먹습니다.");
-    }
-
-    public void Walk()
-    {
-        Console.WriteLine(Name + "이(가) 걷습니다.");
-    }
-
-    public void Run()
-    {
-        Console.WriteLine(Name + "이(가) 뜁니다.");
-    }
-}
-
 namespace ConsoleApp1
 {
+    class Person
+    {
+        public string Name;
+        public string Birthday;
+        public string Gender;
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + "이(가) 아침을 먹습니다.");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine(Name + "이(가) 걷습니다.");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(Name + "이(가) 뜁니다.");
+        }
+    }
+
+    class Cat
+    {
+        public string Name;
+        public int Weight;
+
+        public Cat(string name) {
+            Name = name;
+            Console.WriteLine("고양이의 이름은  "+ Name+ "입니다."); }
+
+        // EXAMPLE: OVERLOADING
+        // 클래스 내부에 동일한 이름의 클래스를 여러 개 선언하고, 필요에 맞게 자동으로 호출할 수 있도록 만들어준다.
+        public Cat(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+
+            Console.WriteLine("고양이의 이름은 " + Name + "이며, 몸무게는 " + Weight + "kg입니다.");
+        } 
+
+        /* 소멸자 Destructor
+         * 메소드 앞에 ~ 기호로 구별한다. 시스템의 가비지 컬렉터에서 소멸자가 호출되는 시점을 결정한다.
+         * 사용하지 않는 메모리 자원을 관리해주는 도구로, 코드 작성 시 메모리 관리를 별도로 하지 않아도 메모리가 부족하면
+         * 가비지 컬렉터가 알아서 사용하지 않는 클래스/변수에서 메모리를 해제하고 필요한 곳에 다시 메모리를 할당하게 관리한다.
+         * 하지만 가비지 컬렉터가 알아서 메모리를 관리하므로 언제 동작할 지 알 수 없고, 소멸자가 호출되는 시점도 알 수 없다.
+         */
+        ~Cat() { Console.WriteLine(Name + "가 사라집니다."); }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+             인스턴스 변수는 일반 변수를 선언하는 것과 유사하지만, 데이터 형이 클래스가 된다. 인스턴스 변수 p1은 Person 클래스를 바탕으로 만들어진다.
+             */
+            //Person p1;
+            //p1 = new Person();
+            //p1.Name = "aa";
+            //p1.Eat();
 
-            Person p1;
-            p1 = new Person();
-            p1.Name = "aa";
-            p1.Eat();
-
+            Cat coco = new Cat("코코");
+            Cat moly = new Cat("몰리", 3);
+            
         }
     }
 }
