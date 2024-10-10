fa55d9cc1f639716f713cce53053ea37f669fabe
syyou <syyou@pintel.co.kr>
2024-10-10 09:59:59 +0900

    FIRST COMMIT: C# if-else if

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*03-2. 프로그램의 흐름을 결정하는 제어문 */
+            // IF-ELSE IF
+            int num = 0;
+
+            if (num > 0) Console.WriteLine("양수");
+            else if (num < 0) Console.WriteLine("음수");
+            else Console.WriteLine("영");
+
+
+        }
+    }
+}
