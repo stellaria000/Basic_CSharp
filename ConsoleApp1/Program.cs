62865b521446ec4dd7d05111f17e5cba287d0d44
syyou <syyou@pintel.co.kr>
2024-10-10 10:08:19 +0900

    Added Incresement/Decresement code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,33 +12,41 @@ namespace ConsoleApp1
         {
             /*03-2. 프로그램의 흐름을 결정하는 제어문 */
             // IF-ELSE IF
-            int num = 0;
+            //int num = 0;
 
-            if (num > 0) Console.WriteLine("양수");
-            else if (num < 0) Console.WriteLine("음수");
-            else Console.WriteLine("영");
+            //if (num > 0) Console.WriteLine("양수");
+            //else if (num < 0) Console.WriteLine("음수");
+            //else Console.WriteLine("영");
 
             // FOR
             // for (int i = 1; i < 11; i++) Console.WriteLine(i);
 
             // CALCULATION
-            int result, num1, num2;
-            result = 3 + 1;
-            Console.WriteLine(result);
+            //int result, num1, num2;
+            //result = 3 + 1;
+            //Console.WriteLine(result);
 
-            result = 3 - 1;
-            Console.WriteLine(result);
+            //result = 3 - 1;
+            //Console.WriteLine(result);
 
-            num1 = 2;
-            result = 5 * num1;
-            Console.WriteLine(result);
+            //num1 = 2;
+            //result = 5 * num1;
+            //Console.WriteLine(result);
 
-            num2 = 10;
-            result = num2 / 3;
-            Console.WriteLine(result);
+            //num2 = 10;
+            //result = num2 / 3;
+            //Console.WriteLine(result);
 
-            result = 10 % 2;
-            Console.WriteLine(result);
+            //result = 10 % 2;
+            //Console.WriteLine(result);
+
+            // INCRESEMENT/DECRESEMENT
+            int num = 0;
+            Console.WriteLine(num++);
+            Console.WriteLine(num);
+            Console.WriteLine(--num);
+            Console.WriteLine(num);
+
         }
     }
 }
