c73c9232df8457b0eebde35bd0816dfdd9f0ed2d
syyou <syyou@pintel.co.kr>
2024-10-10 11:48:38 +0900

    04-1. Class and Instance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,106 +1,136 @@
 ﻿using System;
 
+class Person
+{
+    public string Name;
+    public string Birthday;
+    public string Gender;
+
+    public void Eat()
+    {
+        Console.WriteLine(Name + "이(가) 아침을 먹습니다.");
+    }
+
+    public void Walk()
+    {
+        Console.WriteLine(Name + "이(가) 걷습니다.");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine(Name + "이(가) 뜁니다.");
+    }
+}
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*03-2. 프로그램의 흐름을 결정하는 제어문 */
-            // IF-ELSE IF
-            //int num = 0;
 
-            //if (num > 0) Console.WriteLine("양수");
-            //else if (num < 0) Console.WriteLine("음수");
-            //else Console.WriteLine("영");
+            Person p1;
+            p1 = new Person();
+            p1.Name = "aa";
+            p1.Eat();
 
-            // FOR
-            // for (int i = 1; i < 11; i++) Console.WriteLine(i);
-
-            // WHILE
-            /* int i = 1;
-            while (i < 11) Console.WriteLine(i++); */
-
-            /*03-3. 데이터를 연산자로 요리하기*/
-            // CALCULATION
-            //int result, num1, num2;
-            //result = 3 + 1;
-            //Console.WriteLine(result);
-
-            //result = 3 - 1;
-            //Console.WriteLine(result);
-
-            //num1 = 2;
-            //result = 5 * num1;
-            //Console.WriteLine(result);
-
-            //num2 = 10;
-            //result = num2 / 3;
-            //Console.WriteLine(result);
-
-            //result = 10 % 2;
-            //Console.WriteLine(result);
-
-            // INCRESEMENT/DECRESEMENT
-            //int num = 0;
-            //Console.WriteLine(num++);
-            //Console.WriteLine(num);
-            //Console.WriteLine(--num);
-            //Console.WriteLine(num);
-
-            // COMPARING
-            //bool result;
-            //int num1, num2;
-
-            //num1 = 3;
-            //num2 = 5;
-
-            //result = num1 > num2;
-            //Console.WriteLine(result);
-
-            //result = num1 < num2;
-            //Console.WriteLine(result);
-
-            //result = num1 >= num2;
-            //Console.WriteLine(result);
-
-            //result = num1<= num2;
-            //Console.WriteLine(result);
-
-            //result = num1 == num2;
-            //Console.WriteLine(result);
-
-            //result = num1 != num2;
-            //Console.WriteLine(result);
-
-            // LOGIC(AND/OR/NOT)
-            //bool A, B;
-            //A = true;
-            //B = false;
-
-            // AND
-            //Console.WriteLine(A && A);
-            //Console.WriteLine(A && B);
-
-            //// OR
-            //Console.WriteLine(A || A);
-            //Console.WriteLine(A || B);
-
-            //// NOT
-            //Console.WriteLine(!A);
-            //Console.WriteLine(!B);
-
-            //int score = 85;
-            //if (score > 80)
-            //    if (score <= 100) Console.WriteLine("A");
-
-            /*03-4. 데이터 옮겨 담기*/
-            sbyte value1 = 64;
-            sbyte value2 = 64;
-
-            int value3 = (int)value1 + value2;
-
-            Console.WriteLine(value3);
         }
     }
 }
+
+//        /*03-2. 프로그램의 흐름을 결정하는 제어문 */
+//        // IF-ELSE IF
+//        //int num = 0;
+
+//        //if (num > 0) Console.WriteLine("양수");
+//        //else if (num < 0) Console.WriteLine("음수");
+//        //else Console.WriteLine("영");
+
+//        //// FOR
+//        //for (int i = 1; i < 11; i++) Console.WriteLine(i);
+
+//        //// WHILE
+//        //int i = 1;
+//        //while (i < 11) Console.WriteLine(i++);
+
+//        ///*03-3. 데이터를 연산자로 요리하기*/
+//        //// CALCULATION
+//        //int result, num1, num2;
+//        //result = 3 + 1;
+//        //Console.WriteLine(result);
+
+//        //result = 3 - 1;
+//        //Console.WriteLine(result);
+
+//        //num1 = 2;
+//        //result = 5 * num1;
+//        //Console.WriteLine(result);
+
+//        //num2 = 10;
+//        //result = num2 / 3;
+//        //Console.WriteLine(result);
+
+//        //result = 10 % 2;
+//        //Console.WriteLine(result);
+
+//        //// INCRESEMENT / DECRESEMENT
+//        //int num = 0;
+//        //Console.WriteLine(num++);
+//        //Console.WriteLine(num);
+//        //Console.WriteLine(--num);
+//        //Console.WriteLine(num);
+
+//        //// COMPARING
+//        //bool result;
+//        //int num1, num2;
+
+//        //num1 = 3;
+//        //num2 = 5;
+
+//        //result = num1 > num2;
+//        //Console.WriteLine(result);
+
+//        //result = num1 < num2;
+//        //Console.WriteLine(result);
+
+//        //result = num1 >= num2;
+//        //Console.WriteLine(result);
+
+//        //result = num1 <= num2;
+//        //Console.WriteLine(result);
+
+//        //result = num1 == num2;
+//        //Console.WriteLine(result);
+
+//        //result = num1 != num2;
+//        //Console.WriteLine(result);
+
+//        //// LOGIC(AND / OR / NOT)
+//        //bool A, B;
+//        //A = true;
+//        //B = false;
+
+//        //// AND
+//        //Console.WriteLine(A && A);
+//        //Console.WriteLine(A && B);
+
+//        //// OR
+//        //Console.WriteLine(A || A);
+//        //Console.WriteLine(A || B);
+
+//        //// NOT
+//        //Console.WriteLine(!A);
+//        //Console.WriteLine(!B);
+
+//        //int score = 85;
+//        //if (score > 80)
+//        //    if (score <= 100) Console.WriteLine("A");
+
+//        ///*03-4. 데이터 옮겨 담기*/
+//        //sbyte value1 = 64;
+//        //sbyte value2 = 64;
+
+//        //int value3 = (int)value1 + value2;
+
+//        //Console.WriteLine(value3);
+//        //Console.WriteLine(value3);
