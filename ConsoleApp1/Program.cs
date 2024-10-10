884cf82248788c180170ebe52df5a030c85cfa07
syyou <syyou@pintel.co.kr>
2024-10-10 10:06:30 +0900

    Added calculation code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,7 +19,26 @@ namespace ConsoleApp1
             else Console.WriteLine("영");
 
             // FOR
-            for (int i = 1; i < 11; i++) Console.WriteLine(i);
+            // for (int i = 1; i < 11; i++) Console.WriteLine(i);
+
+            // CALCULATION
+            int result, num1, num2;
+            result = 3 + 1;
+            Console.WriteLine(result);
+
+            result = 3 - 1;
+            Console.WriteLine(result);
+
+            num1 = 2;
+            result = 5 * num1;
+            Console.WriteLine(result);
+
+            num2 = 10;
+            result = num2 / 3;
+            Console.WriteLine(result);
+
+            result = 10 % 2;
+            Console.WriteLine(result);
         }
     }
 }
